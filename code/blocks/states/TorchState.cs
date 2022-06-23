8020032fd0ea0e3ee91492de617f30db571ae302
kurozael <kurozael@gmail.com>
2022-06-23 23:48:16 +0100

    Update shader + torch state

--- code/blocks/states/TorchState.cs
+++ code/blocks/states/TorchState.cs
@@ -5,18 +5,6 @@ namespace Facepunch.Voxels
 {
 	public class TorchState : BlockState
 	{
-		public BlockFace Direction { get; set; }
 		
-		public override void Serialize( BinaryWriter writer )
-		{
-			base.Serialize( writer );
-			writer.Write( (byte)Direction );
-		}
-
-		public override void Deserialize( BinaryReader reader )
-		{
-			base.Deserialize( reader );
-			Direction = (BlockFace)reader.ReadByte();
-		}
 	}
 }
